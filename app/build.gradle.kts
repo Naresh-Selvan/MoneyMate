d45f02f7df8600fab1954cb18e72f1b0f1d349fe
Naresh-Selvan <nareshselvan12@gmail.com>
2026-05-28 08:59:27 +0530

    changed vc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.moneymate.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
+        versionCode = 15
         versionName = "1.1"
     }
 

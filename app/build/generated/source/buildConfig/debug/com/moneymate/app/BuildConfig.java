1e0a95e7ee31fec67b1e8969f23d39ea5b1644b5
Naresh-Selvan <nareshselvan12@gmail.com>
2026-06-09 20:52:05 +0530

    Final

--- app/build/generated/source/buildConfig/debug/com/moneymate/app/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/moneymate/app/BuildConfig.java
@@ -7,7 +7,7 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.moneymate.app";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 56;
+  public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.2";
   // Field from default config.
   public static final String LICENSE_SERVER_URL = "https://your-license-server.com/api/verify";
